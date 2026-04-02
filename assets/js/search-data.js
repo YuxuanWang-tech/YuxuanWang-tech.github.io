3292618b8adccc89f460d393aac724ab3e1027ac
YuxuanWang-tech <82872722+YuxuanWang-tech@users.noreply.github.com>
2026-04-02 15:27:04 +0000

    Deploying to gh-pages from @ YuxuanWang-tech/YuxuanWang-tech.github.io@8303bd184dbef9fc78a3e41e008505ee30dbebc6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -486,9 +486,9 @@ ninja.data = [{
           description: "with background image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
+            },},{id: "projects-beyond-the-code",
+          title: 'Beyond the Code',
+          description: "Structure in Physics, Emotion in Music.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/8_project/";
             },},{id: "projects-project-9",
